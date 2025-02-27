826c7bf1103bf853ad3712a939f1fa42b7c58ff6
Sai Ganesh Ratnala <125180139+saiganesh1976@users.noreply.github.com>
2025-02-27 15:57:25 +0530

    Update assets.js

--- FrontEnd/src/assets/frontend_assets/assets.js
+++ FrontEnd/src/assets/frontend_assets/assets.js
@@ -71,7 +71,7 @@ import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
 export const assets = {
-    logo,
+    // logo,
     basket_icon,
     // header_img,
     search_icon,
